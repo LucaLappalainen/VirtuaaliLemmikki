038a474cb1c68352e81119368ddcbf420e9ddca7
Luca Lappalainen <lappalainen.luca@gmail.com>
2025-01-15 10:34:42 +0200

    demopeli

---
+++ demopeli/src/app/page.tsx
@@ -0,0 +1,34 @@
+"use client"
+import { useEffect } from "react";
+
+
+export default function Home() {
+
+useEffect(() => {
+    const script = document.createElement("script")
+    script.src = "Build\demopeliUusi.loader.js"
+    script.async = true,
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script)
+    }
+
+
+},[])
+
+  return (
+    <div>
+
+      <iframe src="index.html"
+      width="100%"
+      height="100%"
+      style={{border: "none", position: "absolute", top: 0, bottom: 0, left:0, right:0}}
+      allowFullScreen
+      >
+      </iframe>
+
+    </div>
+  );
+}
